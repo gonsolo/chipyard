7eae7edde8e2bde1448706a6abce4a006e59dc76
Gon Solo <gonsolo@gmail.com>
2024-03-20 12:19:06 +0100

    Build and run in GONSOLO.

--- generators/chipyard/src/main/scala/config/TioConfigs.scala
+++ generators/chipyard/src/main/scala/config/TioConfigs.scala
@@ -10,16 +10,6 @@ import freechips.rocketchip.diplomacy.{AsynchronousCrossing}
 class TioConfig extends Config(
   new tio.WithTio ++
   new freechips.rocketchip.subsystem.WithNBigCores(1) ++
+  new freechips.rocketchip.subsystem.WithoutTLMonitors ++         // Faster without Tilelink monitors
   new chipyard.config.AbstractConfig)
-
-//class GCDTLRocketConfig extends Config(
-//  new chipyard.example.WithGCD(useAXI4=false, useBlackBox=false) ++          // Use GCD Chisel, connect Tilelink
-//  new freechips.rocketchip.subsystem.WithNBigCores(1) ++
-//  new chipyard.config.AbstractConfig)
-
-//class InitZeroRocketConfig extends Config(
-//  new chipyard.example.WithInitZero(0x88000000L, 0x1000L) ++   // add InitZero
-//  new freechips.rocketchip.subsystem.WithNBigCores(1) ++
-//  new chipyard.config.AbstractConfig)
-
 
