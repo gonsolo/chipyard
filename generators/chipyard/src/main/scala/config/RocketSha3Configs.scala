14b3156cd4fb43f9ae56c80530fa9005209404ae
Gon Solo <gonsolo@gmail.com>
2024-05-03 18:57:21 +0200

    No sha3 configs.

--- generators/chipyard/src/main/scala/config/RocketSha3Configs.scala
+++ generators/chipyard/src/main/scala/config/RocketSha3Configs.scala
@@ -9,14 +9,14 @@ import freechips.rocketchip.diplomacy.{AsynchronousCrossing}
 // --------------
 
 // DOC include start: Sha3Rocket
-class Sha3RocketConfig extends Config(
-  new sha3.WithSha3Accel ++                                // add SHA3 rocc accelerator
-  new freechips.rocketchip.subsystem.WithNBigCores(1) ++
-  new chipyard.config.AbstractConfig)
-// DOC include end: Sha3Rocket
-
-class Sha3RocketPrintfConfig extends Config(
-  new sha3.WithSha3Printf ++
-  new sha3.WithSha3Accel ++                                // add SHA3 rocc accelerator
-  new freechips.rocketchip.subsystem.WithNBigCores(1) ++
-  new chipyard.config.AbstractConfig)
+//class Sha3RocketConfig extends Config(
+//  new sha3.WithSha3Accel ++                                // add SHA3 rocc accelerator
+//  new freechips.rocketchip.subsystem.WithNBigCores(1) ++
+//  new chipyard.config.AbstractConfig)
+//// DOC include end: Sha3Rocket
+//
+//class Sha3RocketPrintfConfig extends Config(
+//  new sha3.WithSha3Printf ++
+//  new sha3.WithSha3Accel ++                                // add SHA3 rocc accelerator
+//  new freechips.rocketchip.subsystem.WithNBigCores(1) ++
+//  new chipyard.config.AbstractConfig)
